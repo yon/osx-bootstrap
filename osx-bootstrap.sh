19d7037735899f636aa9ab6afc7aa712ee93cce8
Yonatan Feldman <yon@milliped.com>
2018-09-30 01:42:06 -0400

    Remove tmp file

--- osx-bootstrap.sh
+++ osx-bootstrap.sh
@@ -5,7 +5,8 @@ if [ ! -f /Library/Developer/CommandLineTools/usr/bin/gnumake ]; then
   touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
   CLI=$(softwareupdate --list | grep "\* Command Line Tools" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n');
   softwareupdate --install "$CLI" --verbose;
+  rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
 fi
 
-# Run OSX Bootstrap Makefile
+# Run osx-bootstrap Makefile; target default
 curl -fsSL https://raw.githubusercontent.com/yon/osx-bootstrap/master/Makefile | cat | make -
