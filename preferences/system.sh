b2eef224c484d3efd49807f05a916f57afbc4dc5
Yonatan Feldman <yon@users.noreply.github.com>
2018-12-04 01:37:41 -0500

    Set hostname and fix menuExtras

--- preferences/system.sh
+++ preferences/system.sh
@@ -5,6 +5,13 @@ osascript -e 'tell application "System Preferences" to quit'
 sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+read -p 'Enter your desired hostname: ' hostname
+sudo scutil --set ComputerName "${hostname}"
+sudo scutil --set HostName "${hostname}"
+sudo scutil --set LocalHostName "${hostname}"
+sudo systemsetup -setcomputername "${hostname}"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "${hostname}"
+
 defaults write NSGlobalDomain AppleICUForce24HourTime -bool true
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 2
 defaults write NSGlobalDomain AppleLanguages -array 'en-US' 'es-US'
@@ -38,6 +45,7 @@ defaults write NSGlobalDomain QLPanelAnimationDuration -float 0
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 defaults -currentHost write com.apple.screensaver idleTime -int 600
 defaults -currentHost write com.apple.screensaver showClock -bool true
+defaults -currentHost write com.apple.systemuiserver dontAutoLoad -array '/System/Library/CoreServices/Menu Extras/Battery.menu' '/System/Library/CoreServices/Menu Extras/Bluetooth.menu' '/System/Library/CoreServices/Menu Extras/Clock.menu' '/System/Library/CoreServices/Menu Extras/TimeMachine.menu' '/System/Library/CoreServices/Menu Extras/User.menu' '/System/Library/CoreServices/Menu Extras/Volume.menu'
 defaults write ~/Library/Preferences/ByHost/com.apple.coreservices.useractivityd ActivityAdvertisingAllowed -bool true
 defaults write ~/Library/Preferences/ByHost/com.apple.coreservices.useractivityd ActivityReceivingAllowed -bool true
 defaults write AppleEnableSwipeNavigateWithScrolls -bool false
@@ -91,7 +99,8 @@ defaults write com.apple.iTunes disablePingSidebar -bool true
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 defaults write com.apple.loginwindow LoginwindowLaunchesRelaunchApps -bool false
 defaults write com.apple.loginwindow TALLogoutSavesState -bool false
-defaults write com.apple.menuextra.clock DateFormat -string 'HH:mm'
+defaults write com.apple.menuextra.battery ShowPercent -bool true
+defaults write com.apple.menuextra.clock DateFormat -string 'EEE HH:mm'
 defaults write com.apple.notificationcenterui bannerTime -float 2.5
 defaults write com.apple.preferences.extensions.ShareMenu displayOrder -array 'com.apple.share.System.add-to-safari-reading-list' 'com.apple.share.Messages.compose' 'com.apple.share.AirDrop.send' 'com.apple.share.Mail.compose' 'com.apple.share.Facebook.post' 'com.apple.share.Twitter.post'
 defaults write com.apple.print.PrintingPrefs 'Quit When Finished' -bool true
@@ -109,6 +118,7 @@ defaults write com.apple.speech.recognition.AppleSpeechRecognition.prefs Dictati
 defaults write com.apple.speech.recognition.AppleSpeechRecognition.prefs DictationIMSIFolderWasUpdated -bool true
 defaults write com.apple.speech.recognition.AppleSpeechRecognition.prefs DictationIMUseOnlyOfflineDictation -bool true
 defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
+defaults write com.apple.systemuiserver menuExtras -array '/System/Library/CoreServices/Menu Extras/AirPort.menu'
 defaults write com.apple.TextEdit PlainTextEncoding -int 4
 defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
 defaults write com.apple.TextEdit RichText -bool false
@@ -117,35 +127,10 @@ sudo defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool t
 sudo defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 sudo defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
 
-for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.* \
+for process in \
+  'cfprefsd' \
+  'Dock' \
+  'SystemUIServer' \
 ; do
-  sudo defaults write "${domain}" dontAutoLoad -array \
-    '/System/Library/CoreServices/Menu Extras/Battery.menu' \
-    '/System/Library/CoreServices/Menu Extras/Bluetooth.menu' \
-    '/System/Library/CoreServices/Menu Extras/Clock.menu' \
-    '/System/Library/CoreServices/Menu Extras/TimeMachine.menu' \
-    '/System/Library/CoreServices/Menu Extras/User.menu' \
-    '/System/Library/CoreServices/Menu Extras/Volume.menu'
+  killall "${process}" &> /dev/null
 done
-
-for domain in \
-  ~/Library/Preferences/com.apple.systemuiserver.plist \
-  com.apple.systemuiserver \
-; do
-  defaults write ${domain} 'NSStatusItem Visible Siri' -bool false
-  defaults write ${domain} 'NSStatusItem Visible com.apple.menuextra.airplay' -bool false
-  defaults write ${domain} 'NSStatusItem Visible com.apple.menuextra.airport' -bool true
-  defaults write ${domain} 'NSStatusItem Visible com.apple.menuextra.battery' -bool false
-  defaults write ${domain} 'NSStatusItem Visible com.apple.menuextra.clock' -bool false
-  defaults write ${domain} 'NSStatusItem Visible com.apple.menuextra.dwellcontrol' -bool false
-  defaults write ${domain} 'NSStatusItem Visible com.apple.menuextra.textinput' -bool false
-  defaults write ${domain} menuExtras -array '/System/Library/CoreServices/Menu Extras/AirPort.menu'
-done
-
-# for process in \
-#   'cfprefsd' \
-#   'Dock' \
-#   'SystemUIServer' \
-# ; do
-#   killall "${process}" &> /dev/null
-# done
