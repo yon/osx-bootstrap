97a82fd4c945ec11099788efc228d14482c9fff6
Yonatan Feldman <yon@milliped.com>
2023-08-17 14:33:21 -0400

    remove chrome preferences

--- preferences/google-chrome.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
-defaults write com.google.Chrome DisablePrintPreview -bool true
-defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
